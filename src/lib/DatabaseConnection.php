5e047a6490a4bc1345599c502962378f643ab5b6
Axel Villeret <axel_villeret@orange.fr>
2024-09-04 12:45:49 +0200

    chore: Update dependencies, add .env to .gitignore, and configure test script

--- src/lib/DatabaseConnection.php
+++ src/lib/DatabaseConnection.php
@@ -1,13 +1,31 @@
 <?php
 
+require_once 'vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
 class DatabaseConnection
 {
     private ?\PDO $database = null;
 
+    public function __construct()
+    {
+        $projectRoot = dirname(__DIR__, 2);
+        $dotenv = Dotenv::createImmutable($projectRoot);
+        $dotenv->load();
+    }
+
     public function getConnection(): \PDO
     {
         if ($this->database === null) {
-            $this->database = new \PDO('mysql:host=localhost;dbname=perfume-shop;charset=utf8', 'root', '');
+            $host = $_ENV['DB_HOST'];
+            $dbname = $_ENV['DB_NAME'];
+            $charset = $_ENV['DB_CHARSET'];
+            $user = $_ENV['DB_USER'];
+            $password = $_ENV['DB_PASS'];
+
+            $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
+            $this->database = new \PDO($dsn, $user, $password);
         }
 
         return $this->database;
